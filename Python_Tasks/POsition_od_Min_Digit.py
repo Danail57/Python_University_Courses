88a7fe6b4fff253ba4f10474b2720429889c78ae
Danail Donkov <danail.donkov5@gmail.com>
2026-03-28 21:18:28 +0200

    Create POsition_od_Min_Digit.py

---
+++ Python_Tasks/POsition_od_Min_Digit.py
@@ -0,0 +1,19 @@
+###  Да се състави програма,
+# която намира номера на
+# най-малката цифра в
+# зададено цяло число.
+
+def solution(number):
+    number = str(abs(number))
+    min_digit = int(number[0])
+    min_position = 1
+
+    for index, char in enumerate (number):
+        digit = int(char)
+        if digit < min_digit:
+            min_digit = digit
+            min_position = index + 1
+    return min_position
+n = int(input())
+result = solution(n)
+print("The position of the smallest digit:", result)
