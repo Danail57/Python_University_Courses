314a6e74130689f44e521229f2fb08a9e8b73e44
Danail Donkov <danail.donkov5@gmail.com>
2026-03-29 14:29:00 +0300

    Update Count_Symbols.py

--- Python_Tasks/Strings/Count_Symbols.py
+++ Python_Tasks/Strings/Count_Symbols.py
@@ -1,1 +1,13 @@
+###   Да се състави програма,
+# която преброява символите
+# в зададен символен низ.
 
+def count_symbols(string):
+    count = 0
+    for char in string:
+        if char.isalpha():
+            count += 1
+    return count
+
+text = input("Write a string: ")
+print(count_symbols(text))
