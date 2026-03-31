e1da16f6c2dcd56ec31397a945a4d0a9821fb091
Danail Donkov <danail.donkov5@gmail.com>
2026-03-31 14:05:40 +0300

    Update file.py

--- Python_Tasks/ReGex/file.py
+++ Python_Tasks/ReGex/file.py
@@ -1,1 +1,17 @@
+### Write a Python program to validate an email address
+# using a regular expression. Prompt the user to enter
+# an email address and check if it is valid.
 
+import re
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(pattern,email):
+        return True
+    else:
+        return False
+
+user_email = input("Write an email: ")
+if validate_email(user_email):
+    print(f"This email '{user_email}' is valid.")
+else:
+    print(f"This email '{user_email}' is invalid.")
